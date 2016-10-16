b6eaabf6a6da30669ade9ccc073ce94f4c83d840
Philip lambok <philiplambok71@gmai.com>
2016-10-16 13:39:27 +0700

    commit pertama

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Kuliner test</title>
+	<link rel="stylesheet" type="text/css" href="style/style.css">
+</head>
+<body>
+<?php include "navbar.html";?>
+<div class="container">
+<?php include "header.html";?>
+<?php include "content.html"; ?>
+<br class="clear">
+</div>
+<?php include "footer.html"; ?>
+</body>
+</html>
