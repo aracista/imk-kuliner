5b5b75f719ba4bf7d776716c472f0bf6201c8a3a
Philip lambok <philiplambok71@gmai.com>
2016-10-20 10:45:44 +0700

    tambah desain format dasar

---
+++ artikel/drop.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Sandwich kuy</title>
+	<link rel="stylesheet" type="text/css" href="../style/style.css">
+</head>
+<body>
+	<?php include "../navbar.html" ?>
+	<div class="container">
+		<h1>Sandwich KUYY</h1>
+	</div>
+	<?php include "../footer.html" ?>
+</body>
+</html>
