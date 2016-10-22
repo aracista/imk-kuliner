cf68d01e7f0c78cce1e75e99e9e4d42139a3544c
Philiplambok <philiplambok@gmail.com>
2016-10-22 15:42:08 +0700

    model content baru

---
+++ artikel2/baksojoss.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Bakso JOSS</title>
+	<!-- style includer -->
+	<link rel="stylesheet" type="text/css" href="../style/style.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
+	<!-- font here -->
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+	<!-- artikel2 -->
+	<link rel="stylesheet" type="text/css" href="artikel.css">
+
+</head>
+<body>
+<?php include "../navbar.html" ?>
+
+<!-- Arikel -->
+<div class="artikel">
+<!-- Bagian Atas -->
+	<h1>Bakso ONII-SAMA</h1>
+	<hr>
+	<div class="produk">
+		<img src="../images/food/4.jpg">
+	</div>
+	<div class="deskripsi">
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+	</div>
+<!-- End of Bagian Atas -->
+<br class="clear">
+<!-- Bagian Tengah -->
+	<div class="bahan">
+	<h2>Bahan-Bahan</h2>
+		<div class="list">
+			<ul>
+				<li>Garem</li>
+				<li>Garem</li>
+				<li>Garem</li>
+				<li>Garem</li>
+				<li>Garem</li>
+				<li>Garem</li>
+				<li>Garem</li>
+				<li>Bawang bombai</li>
+				<li>Bakso Karin</li>
+				<li>Ramen desu</li>
+			</ul>
+		</div>
+	</div>
+	<br class="clear"> 
+	<div class="caraMemasak">
+	<h2>Cara Memasak</h2>
+		<ol>
+			<li>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</li>
+			<li>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</li>
+			<li>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</li>
+		</ol>
+	</div>
+<!-- End Of Bagian Tengah -->
+<!-- Bagian Bawah -->
+	<div class="video">
+		<video width="100%" controls preload="auto">
+			<source src="../videos/test.mp4" type="video/mp4">
+		</video>
+	</div>
+	<div class="penulis">
+		<div class="images">
+			<img src="../images/user/pquest.jpg">
+		</div>
+		<div class="text">
+			<h3>Reciped by</h3>
+			<p class="username"><a href="#">Admin</a></p>
+			<h3>Bio</h3>
+			<p class="userbio">Not Defined</p>
+		</div>
+	</div>
+<!-- End of Bagian Bawah -->
+<br class="clear">
+</div>
+<!-- end of artikel -->
+
+
+<?php include "../footer.html" ?>
+</body>
+</html>
