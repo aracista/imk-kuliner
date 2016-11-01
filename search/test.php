f4ded86ddf18d1a8e4d3473ddf21f55c1781a116
Philip lambok <philiplambok71@gmai.com>
2016-11-01 10:40:43 +0700

    tambah desain search

---
+++ search/test.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Test Search</title>
+	<link rel="stylesheet" type="text/css" href="../style/style.css">
+	<!-- Icon here -->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
+	<!-- font here -->
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+
+	<!-- style seach -->
+	<link rel="stylesheet" type="text/css" href="search.css">
+</head>
+<body>
+<?php include("../navbar.html") ?>
+
+<div class="container">
+<!-- Judul search -->
+	<p class="searchText">"Kata yang dicari"</p>
+
+<br>
+
+<!-- Hasil search -->
+<div class="hasilSearch">
+	<img src="../images/food/2.jpg">
+	<div class="keteranganSearch">
+		<h2>Burger damn it!</h2>
+		<div class="deskripsi">
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+		</div>
+		<div class="penulis">
+			<img src="../images/user/pquest.jpg">
+			<p>Reciped By pquest</p>
+		</div>
+	</div>
+</div>
+
+<br class="clear">
+
+<div class="hasilSearch">
+	<img src="../images/food/3.jpg">
+	<div class="keteranganSearch">
+		<h2>Watashi wa watashi desu</h2>
+		<div class="deskripsi">
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+		</div>
+		<div class="penulis">
+			<img src="../images/user/rem.png">
+			<p>Reciped By Admin</p>
+		</div>
+	</div>
+</div>
+
+
+<br>
+
+<div class="hasilSearch">
+	<img src="../images/food/2.jpg">
+	<div class="keteranganSearch">
+		<h2>Burger damn it!</h2>
+		<div class="deskripsi">
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+		</div>
+		<div class="penulis">
+			<img src="../images/user/pquest.jpg">
+			<p>Reciped By pquest</p>
+		</div>
+	</div>
+</div>
+
+<br>
+
+</div>
+
+<?php include("../footer.html") ?>
+</body>
+</html>
